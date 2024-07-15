6b392ffc82b00c996d1bdf53b9be91d63284d667
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-16 02:28:37 +0530

    Update code

--- project1/src/app/about/page.js
+++ project1/src/app/about/page.js
@@ -1,14 +1,27 @@
-'use client'
-import React, { useState } from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
 
 const page = () => {
-const [a,b]= useState(0);
+  const [time, setTime] = useState("");
+  useEffect(() => {
+    let interval = setInterval(() => {
+      let realTime = new Date().toLocaleTimeString();
+      setTime(realTime); // Move setTime inside the setInterval callback
+    }, 1000);
+    return () => clearInterval(interval); // Clear interval on component unmount
+  }, []);
   return (
     <div>
-      <h1 >i am about page </h1>
-      <button className=' bg-purple-600 rounded-md px-5 hover:bg-white hover:text-purple-600' onClick={()=> b(a+1)}>Click me {a}</button>
+      <h1>i am about page </h1>
+      <button
+        className=" bg-purple-600 rounded-md px-5 hover:bg-white hover:text-purple-600"
+        onClick={() => b(a + 1)}
+      >
+        Click me{" "}
+      </button>
+      <p>{time}</p>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
